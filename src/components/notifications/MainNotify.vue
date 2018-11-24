6e592e688aa64a5d11a467d7565d3afae8b36932
Eugene Govorun <5797565@gmail.com>
2018-11-24 10:52:35 +0200

    before cherrypick info icon

--- src/components/notifications/MainNotify.vue
+++ src/components/notifications/MainNotify.vue
@@ -9,14 +9,8 @@
         </div>
         <div class="item notification__body--status">
           <div class="icon-wrap">
-            <svg-success v-if="notification.type === 'success'" height="24" width="24">
-            </svg-success>
-            <div v-if="notification.type === 'error' || notification.type === 'info'">
-              <svg fill="#fff" height="48" viewBox="0 0 24 24" width="48" xmlns="http://www.w3.org/2000/svg">
-                <path d="M0 0h24v24H0z" fill="none"></path>
-                <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-2h2v2zm0-4h-2V7h2v6z"></path>
-              </svg>
-            </div>
+            <svg-success v-if="notification.type === 'success'" height="24" width="24"></svg-success>
+            <svg-error v-if="notification.type === 'error' || notification.type === 'info'"></svg-error>
             <svg-warning v-if="notification.type === 'warning'"></svg-warning>
           </div>
         </div>
